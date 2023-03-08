3806e5cf41300e16e5030f50f77b771339f48512
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-08 14:56:39 -0500

    Hadamard

--- Compıtational_Quantum_Mechanics/quantum_simulation.py
+++ Compıtational_Quantum_Mechanics/quantum_simulation.py
@@ -4,3 +4,7 @@ import numpy as np
 qubit1 = np.array([[1], [0]])
 qubit2 = np.array([[1], [0]])
 bell_state = np.kron(qubit1, qubit2) * np.sqrt(2)
+
+
+# create the Hadamard matrix
+hadamard = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
